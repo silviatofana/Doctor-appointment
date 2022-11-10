60b12e1901f0a0511e5dfcb1106817ea581e2347
Felix Ouma <fatonoh@gmail.com>
2022-11-10 19:20:15 +0300

    Add devise gem to the application and update database schema

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      post 'auth/login/', to: 'authentication#login'
     end
   end
 end
