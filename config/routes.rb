e8ae672b4da91cadbd24da4ea2fe655ccac09e25
Henry <dokinruss@gmail.com>
2022-11-14 17:32:29 +0000

    modify the doctor end point

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :doctors, only [:create, :index, :update, :destroy]
+      resources :doctors, only: [:create, :index, :update, :destroy]
       
     end
   end
