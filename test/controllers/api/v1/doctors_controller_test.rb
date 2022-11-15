40bdf438f0858e33caa0a7c7042fbd8c29191b4a
silviatofana <silvia.tofana@gmail.com>
2022-11-15 11:12:43 +0200

    fix linters

--- test/controllers/api/v1/doctors_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class Api::V1::DoctorsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get api_v1_doctors_index_url
-    assert_response :success
-  end
-end
