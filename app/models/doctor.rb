e7641b7dde805f51c48c80e78cbb3f2e80036fd5
Felix Ouma <fatonoh@gmail.com>
2022-11-21 00:32:10 +0300

    Add validation to doctors model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,17 @@
 class Doctor < ApplicationRecord
+  has_many :appointments, dependent: :destroy
+  has_many :users, through: :appointments
+  before_validation :default_values
+  validates :name, presence: true
+  validates :gender, presence: true
+  validates :specialization, presence: true
+
+  def default_values
+    self.photo = if gender == 'M' || gender == 'Male'
+                   'male-placeholder.png'
+                 else
+                   'female-placeholder.png'
+                 end
+    photo
+  end
 end
