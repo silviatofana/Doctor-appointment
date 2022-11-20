ea066fe701d1a222b7c8c7011826e0e9d93dcf78
Felix Ouma <fatonoh@gmail.com>
2022-11-21 00:32:47 +0300

    Update users model to delete dependencies when a user is deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :appointments
+  has_many :appointments, dependent: :destroy
   has_many :doctors, through: :appointments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
