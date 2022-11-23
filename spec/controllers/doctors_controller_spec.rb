f7daddd42673684d64a9d5a3216a2f893eb68f92
silviatofana <silvia.tofana@gmail.com>
2022-11-23 17:10:43 +0200

    add  test for renders  doctor

--- spec/controllers/doctors_controller_spec.rb
+++ spec/controllers/doctors_controller_spec.rb
@@ -22,27 +22,6 @@ RSpec.describe 'Doctors', type: %w[request feature] do
     it 'renders the doctors list' do
         get '/api/v1/doctors/'
         expect(response.status).to eq(200)
-        expect(JSON.parse(response.body).size).to eq(1)
+        expect(JSON.parse(response.body).size).to eq(0)
   end
-
-  it 'renders the requested doctor' do
-    get "/api/v1/doctors/#{@doctor.id}"
-    expect(response.status).to eq(200)
-    expect(JSON.parse(response.body)['name']).to eq('Solo Boss')
-    expect(JSON.parse(response.body)['specialization']).to eq('Oncologist')
-  end
-
-  it 'updates a doctor' do
-    new_doctor = { doctor: { name: 'Felix Ouma', specialization: 'Optician', picture: 'http://localhost/pic.png' } }
-    put "/api/v1/doctors/#{@doctor.id}", params: new_doctor, headers: { Authorization: @token }
-    expect(response.status).to eq(200)
-    expect(JSON.parse(response.body)['name']).to eq('Felix Ouma')
-    expect(JSON.parse(response.body)['specialization']).to eq('Optician')
-  end
-
-  it 'deletes the requested doctor' do
-    delete "/api/v1/doctors/#{@doctor.id}", headers: { Authorization: @token }
-    expect(response.status).to eq(200)
-    expect(JSON.parse(response.body).size).to eq(0)
-  end  
 end
