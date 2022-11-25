00e441cba4b9a0184b5b275fff046091cb26c353
Felix Ouma <fatonoh@gmail.com>
2022-11-25 13:14:15 +0300

    Fix failing tests and add users api documentation

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -1,11 +1,22 @@
 require 'swagger_helper'
+require 'faker'
 
 RSpec.describe 'api/v1/users', type: :request do
 
   path '/api/v1/users' do
 
     get('list users') do
+      parameter({
+      in: :header,
+      type: :string,
+      name: :Authorization,
+      required: true,
+      description: 'Client token'
+    })
+
       response(401, 'successful') do
+        security [Authorization: {}]
+        let(:Authorization) { "Authorization #{:generate_token}" }
 
         after do |example|
           example.metadata[:response][:content] = {
@@ -73,11 +84,15 @@ RSpec.describe 'api/v1/users', type: :request do
   end
 
   path '/api/v1/users/{id}' do
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
     put('update user') do
-      response(403, 'successful') do
-        let(:user) { User.create(name: 'Felix Ouma', gender: 'M', email: "#{DateTime.now}@gmail.com", password: '123456',
+      response(200, 'successful') do
+        consumes 'application/json'
+
+        let(:user) { User.create(name: 'Felix Ouma', gender: 'M', email: Faker::Internet.email, password: '123456',
         password_confirmation: '123456', role: 'admin')}
-        let(:id) { User.create(name: 'Felix Ouma', gender: 'M', email: "#{DateTime.now}@gmail.com", password: '123456',
+        let(:id) { User.create(name: 'Alex Ouma', gender: 'F', email: Faker::Internet.email, password: '123456',
         password_confirmation: '123456', role: 'admin').id}
 
 
@@ -88,6 +103,15 @@ RSpec.describe 'api/v1/users', type: :request do
             }
           }
         end
+        run_test!
+      end
+    end
+
+    delete('delete user') do
+      response(401, 'successful') do
+        let(:id) { User.create(name: 'Felix Ouma', gender: 'M', email: "lauren@gmail.com", password: '123456',
+        password_confirmation: '123456', role: 'admin').id }
+        run_test!
       end
     end
   end
@@ -106,6 +130,13 @@ RSpec.describe 'api/v1/users', type: :request do
           required: %w[email password]
         }
 
+        response(200, 'successful') do
+          unless User.find_by('email': 'herald@gmail.com')
+            User.create(name: 'Felix Ouma', gender: 'M', email: "herald@gmail.com", password: '123456', password_confirmation: '123456', role: 'admin')
+          end
+
+        let(:user) { { email: 'herald@gmail.com', password: '123456'}}
+
         after do |example|
           example.metadata[:response][:content] = {
             'application/json' => {
@@ -113,23 +144,8 @@ RSpec.describe 'api/v1/users', type: :request do
             }
           }
         end
+        run_test!
       end
-    end
-  end
-
-  path '/api/v1/users/{id}' do
-    delete('delete user') do
-      response(401, 'successful') do
-        let(:id) { User.create(name: 'Felix Ouma', gender: 'M', email: "#{DateTime.now}@gmail.com", password: '123456',
-        password_confirmation: '123456', role: 'admin').id }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
       end
     end
   end
